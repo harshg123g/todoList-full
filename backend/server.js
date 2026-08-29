32fa0ce0bcf785a33121046dcb0651c8d6d872d9
harshg123g <2023pcecrharsh025@poornima.org>
2026-08-29 19:00:36 +0530

    Fix backend deployment

--- backend/server.js
+++ backend/server.js
@@ -5,8 +5,8 @@ require('dotenv').config();
 
 connectDB();
 
+const PORT = process.env.PORT || 3000;
 
-
-app.listen(3000, ()=>{
-    console.log('Server is running on 3000')
-})
+app.listen(PORT, '0.0.0.0', () => {
+    console.log(`Server is running on ${PORT}`);
+});
--- backend/src/app.js
+++ backend/src/app.js
@@ -2,21 +2,19 @@ const express = require('express');
 const cors = require('cors');
 const todoRoute = require('./routes/todo.routes');
 
-// const authRouter = require('./routers/auth.routes')
-// const musicRouter = require('./routers/music.routes')
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 
-
-// app.get('/api', (req, res) =>{
-//     res.send("TodoList from app.js");
-// })
+app.get('/', (req, res) => {
+    res.json({
+        message: 'Todo List API is running'
+    });
+}); 
 
 
 app.use('/api/todo', todoRoute)
-// app.use('/api/music', musicRouter)
 
 
 
